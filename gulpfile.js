8e89c48f977ff6f780a87d9fb84fac6f7b1c4ec6
Duy <truongvanduy09072002@gmail.com>
2022-06-07 21:10:24 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug-3');
+const browserSync = require('browser-sync').create();
+
+// Run Sass
+gulp.task('sass', () =>
+  gulp.src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css/'))
+    .pipe(browserSync.stream())
+)
+
+
+// Run pug
+gulp.task('pug', () =>
+  gulp.src('./src/views/*.pug')
+    .pipe(
+      pug({
+        pretty: true,
+      }))
+    .pipe(gulp.dest('./public/views/'))
+    .pipe(browserSync.stream())
+);
+
+// Minify images
+gulp.task('minify images', () =>
+  gulp.src('./src/images/**/*.*')
+    // .pipe(imagemin())
+    .pipe(gulp.dest('./public/images/'))
+);
+
+// Copy js
+gulp.task('copy js', () =>
+  gulp.src('./src/js/**/*.js')
+    .pipe(gulp.dest('./public/js/'))
+);
+
+// Run tasks
+gulp.task('run', gulp.series('pug', 'sass', 'minify images', 'copy js'));
+
+// Watch
+gulp.task('watch', () => {
+  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/views/**/*.pug', gulp.series('pug'));
+  gulp.watch('./src/images/**/*.*', gulp.series('minify images'));
+  gulp.watch('./src/js/**/*.js', gulp.series('copy js'));
+})
+
+// Run all
+gulp.task('default', gulp.series('run', 'watch'));
+
